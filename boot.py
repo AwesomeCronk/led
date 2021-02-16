eec9c8f490bae491dd2f092b42f88738e7144dbe
Clayton Cronk <awesomecronk@gmail.com>
2021-02-15 19:54:09 -0600

    Initial commit.

---
+++ boot.py
@@ -0,0 +1,7 @@
+#Initialize the SD card
+#Connect to WiFi
+#Start the interpreter cycle
+#`-This will occur when main.py runs.
+
+
+print('boot.py running')
---
+++ main.py
@@ -0,0 +1,9 @@
+#Open a TCP socket and listen. When there is a connection, do the following:
+#Get commands
+#Parse commands
+#Take actions
+#send results to client
+#If the client is still there, get the next set of commands and repeat. Otherwise, wait for a new client.
+
+
+print('main.py running')
