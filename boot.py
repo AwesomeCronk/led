990326746e880a6a82b91430317e8a1ec38cd189
Clayton Cronk <awesomecronk@gmail.com>
2021-02-19 22:04:30 -0600

    gitBack autocommit on 02-19-2021 at 22:04:08

--- boot.py
+++ boot.py
@@ -1,7 +1,25 @@
-#Initialize the SD card
+
 #Connect to WiFi
+#Set internal clock with ntptime
 #Start the interpreter cycle
 #`-This will occur when main.py runs.
 
+import gc, network, utime, ntptime
+from machine import Pin, PWM
 
-print('boot.py running')
+g.collect()
+
+station = network.WLAN(network.STA_IF)
+station.active(True)
+station.connect(ssid, password)
+while not station.isconnected():
+    pass
+
+ntptime.host = 'us.pool.itp.org'
+ntptime.settime()
+
+
+pinR = Pin(15)
+pwmR = PWM(pinR)
+pwmR.freq(500)
+pwmR.duty(1023)
