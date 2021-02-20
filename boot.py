9814a6dc77c15ed1de0a061b5d91217a07edfec9
Clayton Cronk <awesomecronk@gmail.com>
2021-02-19 22:10:55 -0600

    Laid out a bit of the general program flow.

--- boot.py
+++ boot.py
@@ -7,18 +7,25 @@
 import gc, network, utime, ntptime
 from machine import Pin, PWM
 
+#Garbage collection, just in case
 g.collect()
 
+#Connect to WiFi
 station = network.WLAN(network.STA_IF)
 station.active(True)
 station.connect(ssid, password)
 while not station.isconnected():
     pass
 
+print('Connected to {}'.format(ssid))
+print(station.ifconfig())
+
+#Update internal time via NTP
 ntptime.host = 'us.pool.itp.org'
 ntptime.settime()
 
-
+#Initialize LEDs. IO15 - Red, IO13 - Green, IO12 - Blue
+valR = 0
 pinR = Pin(15)
 pwmR = PWM(pinR)
 pwmR.freq(500)
